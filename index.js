d3cccafa443cf02e225b80189e06fa19903dede8
github username <jonathansychan@gmail.com>
2020-03-01 18:33:09 -0500

    Logs you in

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const puppeteer = require('puppeteer');
 
 //   Wait some time 
   await page.waitFor(() => document.querySelectorAll('input').length);
-    console.log(process.env.IG_ID)
-  await page.type('[name=username]', process.env.IG_ID, {delay: 15});
-  await page.type('[name=password]', process.env.IG_PASSWORD, {delay: 20});
 
+  await page.type('[name=username]', process.env.IG_ID, {delay: 20});
+  await page.type('[name=password]', process.env.IG_PASSWORD, {delay: 30});
+
+  await page.waitFor(1235);
+  await page.click('button[type="submit"]');
+
+  await page.waitFor(10000);
 
   await browser.close();
 })();
