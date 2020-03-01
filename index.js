c627185ecace1f1bc2ddd4a38de54d4982633a46
github username <jonathansychan@gmail.com>
2020-03-01 18:12:09 -0500

    More targets input fields

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -6,7 +7,12 @@ const puppeteer = require('puppeteer');
   const url = "https://www.instagram.com/accounts/login/?source=auth_switcher";
   await page.goto(url);
 
-  await page.waitFor(3000);
+//   Wait some time 
+  await page.waitFor(() => document.querySelectorAll('input').length);
+    console.log(process.env.IG_ID)
+  await page.type('[name=username]', process.env.IG_ID, {delay: 15});
+  await page.type('[name=password]', process.env.IG_PASSWORD, {delay: 20});
+
 
   await browser.close();
 })();
