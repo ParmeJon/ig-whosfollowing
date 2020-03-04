bb4c7d3aa59b7fd91ac3b90afa3700a247277c44
github username <jonathansychan@gmail.com>
2020-03-03 22:30:28 -0500

    Automates logs myFollowing

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-  const myFollowers = []
-  const myFollowing = []
-  const browser = await puppeteer.launch({headless: false});
-  try {
+    const browser = await puppeteer.launch({headless: false});
+    try {
       const page = await browser.newPage();
       const url = "https://www.instagram.com/accounts/login/?source=auth_switcher";
       await page.goto(url);
@@ -17,7 +15,7 @@ const puppeteer = require('puppeteer');
       await page.type('[name=username]', process.env.IG_ID, {delay: 20});
       await page.type('[name=password]', process.env.IG_PASSWORD, {delay: 30});
     
-      await page.waitFor(1235);
+      await page.waitFor(3250);
       await page.click('button[type="submit"]');
       await page.waitFor(".aOOlW");
       await page.evaluate(() => {
@@ -28,14 +26,46 @@ const puppeteer = require('puppeteer');
       await page.waitFor('.-nal3', {delay: 20});
       await page.evaluate(() => {
           const headerTabs = document.getElementsByClassName('-nal3');
-          headerTabs[1].click()
+          headerTabs[2].click()
       })
-      await page.waitFor(".PZuss");
-      await page.waitFor(10000);
-   
+      await page.waitFor(5000);
+      await scrollToEnd(page, 39);
+      const myFollowing = await page.evaluate(() => {
+          const scrollable_div = document.getElementsByClassName("PZuss")[0]
+          const followingList = scrollable_div.children
+          const followingListArr = [...followingList].map(liElement => liElement.innerText.split(/(\r\n|\n|\r)/gm)[0]);
+          return followingListArr
+      });
+      const myFollowers = await page.evaluate(() => {
+        const scrollable_div = document.getElementsByClassName("PZuss")[0];
+        const followersList = scrollable_div.children;
+        const followersListArr = [...followersList].map(
+          liElement => liElement.innerText.split(/(\r\n|\n|\r)/gm)[0]
+        );
+        return followersListArr;
+      });
+      console.log('MY FOLLOWERS', myFollowing);
+      console.log('I AM FOLLOWING', myFollowing);
   } catch (err) {
       console.error(err.message);
   } finally {
       await browser.close();
   }
 })();
+
+async function scrollToEnd(page, numberOfItems) {
+    let items = 0
+    try {
+        while (items < numberOfItems) {
+            console.log('scrolling')
+            items = await page.evaluate(() => {
+                let scrollable_div = document.getElementsByClassName("PZuss")[0];
+                scrollable_div.scrollIntoView({ block: "end" });
+                return document.getElementsByClassName("PZuss")[0].children.length
+            })
+            await page.waitFor(2000);
+        }
+    } catch(err) {
+        console.error(err)
+    }
+}
